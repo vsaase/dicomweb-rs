67898d25e42440136ebae1241a80824398bc25b9
Victor Saase <vsaase@gmail.com>
2021-05-08 17:49:27 +0200

    builder pattern for client

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,6 +1,10 @@
 use dicom::core::Tag;
+use http;
 use reqwest;
+use reqwest::header::{HeaderName, HeaderValue};
 use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::future::Future;
 
 #[derive(Default)]
 pub struct DICOMWebClient {
@@ -21,27 +25,12 @@ impl DICOMWebClient {
         }
     }
 
-    pub fn query_studies(&self) -> QueryBuilder {
+    pub fn find_studies(&self) -> QueryBuilder {
         QueryBuilder {
             client: &self,
             request_builder: self.client.get(&self.url),
         }
     }
-
-    // pub async fn query_studies(self, query: QueryParameters) -> Result<(), reqwest::Error> {
-    //     let client = reqwest::Client::new();
-    //     let mut req = client.get(self.url).query(&query.values);
-    //     if let Some(limit) = query.limit {
-    //         req = req.query(&[("limit", limit)]);
-    //     }
-    //     let res = req.send().await?;
-    //     println!("Status: {}", res.status());
-    //     println!("Headers:\n{:#?}", res.headers());
-
-    //     let body = res.text().await?;
-    //     println!("Body:\n{}", body);
-    //     Ok(())
-    // }
 }
 
 pub struct QueryBuilder<'a> {
@@ -49,28 +38,32 @@ pub struct QueryBuilder<'a> {
     request_builder: reqwest::RequestBuilder,
 }
 
-pub struct QueryParameters {
-    values: HashMap<String, String>,
-    includefield: Option<Vec<Tag>>,
-    fuzzymatching: Option<bool>,
-    limit: Option<i64>,
-    offset: Option<i64>,
-}
+impl<'a> QueryBuilder<'a> {
+    pub fn header<K, V>(mut self, key: K, value: V) -> QueryBuilder<'a>
+    where
+        HeaderName: TryFrom<K>,
+        <HeaderName as TryFrom<K>>::Error: Into<http::Error>,
+        HeaderValue: TryFrom<V>,
+        <HeaderValue as TryFrom<V>>::Error: Into<http::Error>,
+    {
+        self.request_builder = self.request_builder.header(key, value);
+        self
+    }
 
-impl QueryParameters {
-    pub fn patient(name: &str) -> QueryParameters {
-        let mut values = HashMap::new();
-        values.insert(String::from("00100010"), String::from(name));
-        QueryParameters {
-            values,
-            includefield: None,
-            fuzzymatching: None,
-            limit: None,
-            offset: None,
-        }
+    pub fn patient_name(mut self, name_query: &'a str) -> QueryBuilder {
+        self.request_builder = self.request_builder.query(&[("PatientName", name_query)]);
+        self
+    }
+
+    pub fn limit(mut self, limit: u32) -> QueryBuilder<'a> {
+        self.request_builder = self.request_builder.query(&[("limit", limit.to_string())]);
+        self
+    }
+
+    pub fn send(self) -> impl Future<Output = Result<reqwest::Response, reqwest::Error>> {
+        self.request_builder.send()
     }
 }
-
 #[cfg(test)]
 mod tests {
     #[test]
--- example/src/main.rs
+++ example/src/main.rs
@@ -1,7 +1,7 @@
+use client::DICOMWebClient;
+use error_chain::error_chain;
 use reqwest;
 use serde_json::Value;
-
-use error_chain::error_chain;
 
 error_chain! {
     foreign_links {
@@ -15,22 +15,18 @@ error_chain! {
 async fn main() -> Result<()> {
     // let url = "http://localhost:8088/rs";
     let url = "http://hackathon.siim.org/dicomweb/studies";
-    // let client = DICOMWebClient::new(url);
-    // let query = QueryParameters::patient("Master*");
-    // client.query_studies(query).await?;
-    let client = reqwest::Client::new();
+    let client = DICOMWebClient::new(url);
     let res = client
-        .get(url)
+        .find_studies()
         .header("apikey", "9c8a1e06-9b19-4e36-81ff-3ece53bdb674")
-        .query(&[("PatientName", "*"), ("limit", "10")])
+        .patient_name("*")
+        .limit(10)
         .send()
         .await?;
     println!("Status: {}", res.status());
     println!("Headers:\n{:#?}", res.headers());
 
-    let body = res.text().await?;
-    println!("Body:\n{}", body);
-    let json: Value = serde_json::from_str(&body)?;
+    let json: Value = res.json().await?;
     println!("JSON body:\n{}", json);
     Ok(())
 }
