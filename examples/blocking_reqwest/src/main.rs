f2b0c8568b663617df48892c6f85163c3bdcb5fa
Victor Saase <vsaase@gmail.com>
2021-09-12 13:08:38 +0200

    .

--- examples/blocking_reqwest/src/main.rs
+++ examples/blocking_reqwest/src/main.rs
@@ -17,8 +17,8 @@ fn main() -> Result<()> {
 
     // let url = "http://localhost:8088/rs";
     // let url = "http://hackathon.siim.org/dicomweb";
-    // let url = "http://localhost:8042/dicom-web";
-    let url = "http://localhost:8080/dcm4chee-arc/aets/DCM4CHEE/rs";
+    let url = "http://localhost:8042/dicom-web";
+    // let url = "http://localhost:8080/dcm4chee-arc/aets/DCM4CHEE/rs";
     // let client = DICOMWebClient::new(url);
     info!("creating client");
     let mut client = DICOMWebClientBlocking::new(url)
